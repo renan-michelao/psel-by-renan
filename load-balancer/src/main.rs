2de4a41ae819cbf74326885c96c83880eab2e99c
Renan Michelão <renanmichelao@gmail.com>
2026-08-11 14:34:44 -0300

    Feat: Implementa função básica que processa a requisição

--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -1,4 +1,31 @@
-use std::net::{TcpListener};
+use std::io::Read;
+
+// Importa os componentes de rede 
+use std::net::{TcpListener, TcpStream};
+
+fn processa_clinte(mut stream: TcpStream){
+    // Buffer de 4k preenchido com zeros
+    let mut buffer = [0; 4096];
+
+    // Lê os dados do socket e coloca no buffer
+    match stream.read(&mut buffer){
+        Ok(bytes_lidos) => {
+            if bytes_lidos == 0 {
+                println!("Conexão fechada.");
+                return;
+            }
+
+            // Converte os bytes para uma string
+            let requisicao = String::from_utf8_lossy(&buffer[..bytes_lidos]);
+
+            println!("---- Nova Requisição ----");
+            println!("{}", requisicao);
+        }
+        Err(e) => {
+            println!("Erro ao ler do socket: {}", e);
+        }
+    }
+}
 
 fn main(){
     // Define o endereço e a porta que o load balancer vai escutar
@@ -12,6 +39,7 @@ fn main(){
         match stream{
             Ok(stream) => {
                 println!("Nova conexão");
+                processa_clinte(stream);
             }
             Err(e) => {
                 println!("Erro ao aceitar conexão: {}", e);
