a779b31d3fbd2f11ce707012b23c0853b3665db1
Renan Michelão <renanmichelao@gmail.com>
2026-08-13 21:18:20 -0300

    Feat: Load Balancer conecta ao backend e processa sua resposta

--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -3,12 +3,12 @@ use std::io::{Read, Write};
 // Importa os componentes de rede 
 use std::net::{TcpListener, TcpStream};
 
-fn processa_clinte(mut stream: TcpStream){
+fn processa_clinte(mut client_stream: TcpStream){
     // Buffer de 4k preenchido com zeros
     let mut buffer = [0; 4096];
 
-    // Lê os dados do socket e coloca no buffer
-    match stream.read(&mut buffer){
+    // Lê os dados (requisição) do socket e coloca no buffer
+    match client_stream.read(&mut buffer){
         Ok(bytes_lidos) => {
             if bytes_lidos == 0 {
                 println!("Conexão fechada.");
@@ -21,18 +21,49 @@ fn processa_clinte(mut stream: TcpStream){
             println!("---- Nova Requisição ----");
             println!("{}", requisicao);
 
-            // Monta a string da resposta HTTP
-            let status_line = "HTTP/1.1 200 OK\r\n";
-            let blank_line = "\r\n";
-            let body = "<html><body><h1>Load Balancer rodando nessa quack...</h1></body></html";
+            // Conecta ao servidor backend
+            let endereco_backend = "127.0.0.1:8081"; // Vou usar a porta 8081 para o backend
+            let mut backent_stream = match TcpStream::connect(endereco_backend){
+                Ok(stream) => {
+                    println!("Backend conectado");
+                    stream
+                }
+                Err(e) => {
+                    println!("Erro ao conectar no backend {}", e);
+                    return;
+                }
+            };
 
-            // Junta tudo
-            let resposta = format!("{}{}{}", status_line, blank_line, body);
-
-            // Escreve a resposta no socket
-            if let Err(e) = stream.write_all(resposta.as_bytes()){
-                println!("Erro ao enviar a resposta: {}", e);
+            // Repassa os bytes da requisição para o backend
+            if let Err(e) = backent_stream.write_all(&buffer[..bytes_lidos]) {
+                println!("Erro ao enviar requisição para o back: {}", e);
+                return;
             }
+
+            // Lê a resposta que o backend gerou
+            let mut buffer_resposta = [0; 4096]; // Buffer para a resposta do back
+            let bytes_resposta = match backent_stream.read(&mut buffer_resposta){
+                Ok(bytes) =>{
+                    if bytes == 0{
+                        println!("Conexão do backend fechada");
+                        return;
+                    }
+
+                    println!("Resposta gerada (funciona pelo amor de Deus");
+
+                    bytes // retorna o valor para o 'let bytes_resposta'
+                }
+                Err(e) => {
+                    println!("Erro ao ler resposta do backend: {}", e);
+                    return;
+                }
+            };
+
+            // Devolve a resposta do backend para o cliente
+            if let Err(e) = client_stream.write_all(&buffer_resposta[..bytes_resposta]){
+                println!("Erro ao devolver resposta para o cliente: {}", e);
+            }
+
         }
         Err(e) => {
             println!("Erro ao ler do socket: {}", e);
@@ -52,6 +83,7 @@ fn main(){
         match stream{
             Ok(stream) => {
                 println!("Nova conexão");
+
                 processa_clinte(stream);
             }
             Err(e) => {
