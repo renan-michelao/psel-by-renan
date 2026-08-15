7fba2e9e48fa9a96c29efd0e781ba22a4059f6cd
Renan Michelão <renanmichelao@gmail.com>
2026-08-15 20:08:59 -0300

    Feat: Backend extrai caminho do arquivo e abre

--- load-balancer/src/bin/backend.rs
+++ load-balancer/src/bin/backend.rs
@@ -1,22 +1,77 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::fs;
+
+
+fn extrai_caminho_arquivo(requisicao: &str) -> String   {
+    if let Some(primeira_linha) = requisicao.lines().next() {
+
+        // Divide a linha em pedaços usando espaços em branco
+        let partes: Vec<&str> = primeira_linha.split_whitespace().collect();
+
+        // garante que a requisição está bem formatada (pelo menos método e caminho)
+        if partes.len() >= 2 {
+            let metodo = partes[0];
+            let caminho = partes[1];
+
+            println!("Método: {}", metodo);
+            println!("Caminho: {}", caminho);
+
+
+            if caminho == "/" {
+                return String::from("index.html"); // retorna um arquivo padrão
+            } else{
+                return caminho[1..].to_string();
+            }
+        }
+
+    }
+
+    // Retorno padrão caso a requisição esteja errada
+    String::from("404.html")
+}
 
 fn backend_cabuloso(mut stream: TcpStream) {
     let mut buffer = [0; 4096];
-    
+
     if let Ok(bytes_lidos) = stream.read(&mut buffer) {
-        if bytes_lidos > 0 {
-            println!("Backend recebeu um quack (requisição)");   
+        if bytes_lidos > 0{
+            // Converte a requisição para um string
+            let requisicao_str = String::from_utf8_lossy(&buffer[..bytes_lidos]);
 
-            let status_line = "HTTP/1.1 200 OK\r\n";
-            let blank_line = "\r\n";
-            let body = "<html><body><h1>Toma essa resposta quack (censura)</h1></body></html>";
+            // Extrai qual arquivo o navegador está pedindo
+            let nome_do_arquivo = extrai_caminho_arquivo(&requisicao_str);
+            println!("Backend vai tentar ler: {}", nome_do_arquivo);
 
-            let resposta = format!("{}{}{}", status_line, blank_line, body);
+            match fs::read(&nome_do_arquivo) {
+                Ok(conteudo_do_arquivo) => {
+                    println!("Arquivo encontrado. manda o bglh pro load balancer");
 
-            // Escreve a resposta de volta para o socket (envia de volta para o balancer)
-            if let Err(e) = stream.write_all(resposta.as_bytes()){
-                println!("Erro ao enviar resposta do back: {}", e);
+                    let cabecalho = "HTTP/1.1 200\r\n\r\n";
+
+                    let mut resposta_completa = cabecalho.as_bytes().to_vec();
+                    resposta_completa.extend(conteudo_do_arquivo);
+
+                    // Escreve tudo de volta no socket
+                    if let Err(e) = stream.write_all(&resposta_completa) {
+                        println!("Erro ao enviar a resposta: {}", e);
+                    }
+
+                }
+                Err(_) => {
+                    println!("Arquivo não encontrado. 404");
+
+                    let cabecalho = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+                    let body_error = "<html><body><h1>Erro 404: o pato nao encontrou o bglh</h1></body></html>";
+
+                    // Junta tudo
+                    let resposta_erro = format!("{}{}", cabecalho, body_error);
+
+                    if let Err(e) = stream.write_all(resposta_erro.as_bytes()){
+                        println!("Erro ao enviar erro 404: {}", e);
+                    }
+                }
             }
         }
     }
